f472a92a8cf8350a3ef842c6a7bd7f32d8a2d5fe
takealook2 <a1vm5h@gmail.com>
2021-07-18 23:04:20 +0900

    7/18 heroku 수정

--- lionproject/settings.py
+++ lionproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG','True') != 'False')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [ '*' ]
 
 AUTH_USER_MODEL = 'account.CustomUser'
 
