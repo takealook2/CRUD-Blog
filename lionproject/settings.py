c43ba4dbad2ed1694a8a6f00ca89589fee371360
takealook2 <a1vm5h@gmail.com>
2021-07-18 22:35:39 +0900

    오타 수정 7/18

--- lionproject/settings.py
+++ lionproject/settings.py
@@ -144,7 +144,7 @@ MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = 'AWS_ACCESS_KEY_ID'
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SCRET_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'likelion-django-project'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_REGION_NAME = 'ap-northeast-2'
